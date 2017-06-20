997c581ca5d7584ee56485eb33fbaa90a4a7a6eb
mmarcus11 <mmarcus@brandeis.edu>
2017-06-20 01:15:03 -0400

    Converting to Personal App

--- client/templates.js
+++ client/templates.js
@@ -3,19 +3,34 @@ Template.showpeople.helpers({
 })
 
 Template.addPerson.events({
-    'click button'(elt,instance) {
+    'click button'(elt, instance) {
         const name = instance.$('#name').val();
         const email = instance.$('#email').val();
         console.log('adding '+name);
         instance.$('#name').val("");
         instance.$('#email').val("");
         Roommate.insert({name:name, email:email});
-        //People. insert({name, birthyear})
+
     }
 })
 
 Template.personrow.events({
     'click span'(elt,instance) {
-        Roommate.remove(this.roommate._id);
+        Roommate.remove(this.person._id);
+        //if (this.person.ownder==Meteor.userID()){
+          //People.remoce(this.person._id);
+        //}
     }
+})
+
+Template.chorerow.helpers({
+    choreslist() {return Chores.find()},
+})
+
+Template.addTask.events({
+    'click button'(elt,instance){
+        const type = instance.$('#type').val();
+        Chores.insert({type:type});
+    }
+
 })
--- server/startup.js
+++ server/startup.js
@@ -2,7 +2,10 @@ import { Meteor } from 'meteor/meteor';
 
 Meteor.startup((/*arguments would go here*/) => { /*start content*/
   // code to run on server at startup
-  Roommate.remove({});  // clear the database
-  Roommate.insert({name:'Tim', email:"asdfhs@shfd.com", chores:""});
-  Roommate.insert({name:'Caitlin', email:"asdfhs@shfd.com", chores:""});
+//   ServiceConfiguration.configurations.remove({
+//   service: "google"
+// });
+  // Roommate.remove({});  // clear the database
+  // Roommate.insert({name:'Tim', email:"asdfhs@shfd.com"});
+  // Roommate.insert({name:'Caitlin', email:"asdfhs@shfd.com"});
 }/*end content*/);
