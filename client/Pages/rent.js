0db1ce40a3023ef49fa27c5ee1234360351e3f72
mmarcus11 <mmarcus@brandeis.edu>
2017-06-27 14:52:13 -0400

    person rent

--- client/Pages/rent.js
+++ client/Pages/rent.js
@@ -1,12 +1,10 @@
 const house =
-  [
-    {name: "Roommate 1"},
-    {name: "Roommate 2"},
-    {name: "Roommate 3"}
-  ];
+
 
 Template.rent.helpers(
  {
-   house,
+   house: function(){
+     return Roommate.find();
+   }
  }
 )
